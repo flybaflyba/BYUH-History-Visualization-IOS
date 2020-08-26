05fad44b6f1b6c5dc7f248bb035bfd5d4da823f8
Litian Zhang <litian_zhang17@163.com>
2020-08-26 10:57:34 -1000

    Create new project

---
+++ BYUH-History-Visualization-IOS/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BYUH-History-Visualization-IOS
+//
+//  Created by tiantian on 2020/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
